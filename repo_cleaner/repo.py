754b15ef2960703005588771e0b8df3a20cd11bf
Andrew Luchuk <andrew.luchuk@outlook.com>
2022-10-08 13:50:18 -0400

    begins adding repo class

---
+++ repo_cleaner/repo.py
@@ -0,0 +1,12 @@
+class Repo:
+
+    def __init__(self, raw_data):
+        self.owner = raw_data['owner']['login']
+        self.name = raw_data['name']
+        self.is_fork = 'fork' in raw_data and raw_data['fork']
+        self.parent_set = False
+        self.url = raw_data['url']
+
+        if self.is_fork and 'parent' in raw_data:
+            self.parent_set = True
+            self.parent_owner = raw_data['parent']['owner']['login']
