1774c9722e8f51f378a583f048295714e51277ad
Andrew Luchuk <andrew.luchuk@outlook.com>
2022-10-17 22:46:24 -0400

    adds init main

--- repo_cleaner/__init__.py
+++ repo_cleaner/__init__.py
@@ -1,8 +1,21 @@
 import os
+import sys
+
+from .cleaner import Cleaner
 
 
 GHUB_TOKEN_VAR = 'GITHUB_TOKEN'
 
 
 def load_github_token():
-    return os.environ.get(GHUB_TOKEN_VAR)
+    token_result = os.environ.get(GHUB_TOKEN_VAR)
+    if token_result is not None:
+        return token_result
+    else:
+        return ''
+
+
+if __name__ == '__main__':
+    cleaner = Cleaner(sys.argv, load_github_token())
+    cleaner.run()
+
